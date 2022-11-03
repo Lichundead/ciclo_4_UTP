002c3e35d3804b5f983e02b0ae594428d00c4f22
Juan José Rivera <111877277+juanRpy@users.noreply.github.com>
2022-11-03 09:14:01 -0500

    Create login_admin.controller.js

---
+++ panoptico_node/controllers/login_admin.controller.js
@@ -0,0 +1,26 @@
+const Admin= require('../models/login_admin.model');
+let response = {
+    msg: "",
+    exito: false,
+};
+
+exports.create = function (req, res) {
+    let admin = new Admin({      
+        email: req.body.email,
+        contraseña: req.body.contraseña,
+    });
+
+    visitante.save(function (err) {
+        if (err) {
+            console.error(err),
+            (response.exito = false),
+            (response.msg = "Error al guardar el admin");
+        res.json(response);
+        return;
+    }
+
+        (response.exito = true),
+            (response.msg = "El admin se guardó correctamente");
+        res.json(response);
+    });
+};
