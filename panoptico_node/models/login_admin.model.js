bef5d452aaa612d2bfe240a07a79d5a06198d7d1
Juan José Rivera <111877277+juanRpy@users.noreply.github.com>
2022-11-05 14:22:17 -0500

    Update login_admin.model.js

--- panoptico_node/models/login_admin.model.js
+++ panoptico_node/models/login_admin.model.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const  Login_adminSchema = new Schema ({
-    //Por el momento no utilizar campo autoincrementable
-    //id_ingreso: { type: Number, required: true, max: 1000 },
     email:{type: String, required: false, max: 70},
     contraseña: {type: String, required: false, max: 70},
 });
