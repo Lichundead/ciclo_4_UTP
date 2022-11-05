438eba7d605c38953238580ba8fd9494230c9985
Juan José Rivera <111877277+juanRpy@users.noreply.github.com>
2022-11-05 14:25:49 -0500

    Update login_admin.router.js

--- panoptico_node/routes/login_admin.router.js
+++ panoptico_node/routes/login_admin.router.js
@@ -3,5 +3,10 @@ const router = express.Router();
 const login_adminController = require("../controllers/login_admin.controller");
 
 router.post("/", login_adminController.create);
+router.get("/", login_adminController.find);
+router.get("/:id", login_adminController.findOne);
+router.put("/:id", login_adminController.update);
+router.delete("/:id", login_adminController.remove);
+
 
 module.exports = router;
