394acfb9f0f27bbbd733cedc2daf544e3eeba96b
Juan José Rivera <111877277+juanRpy@users.noreply.github.com>
2022-11-06 21:57:51 -0500

    Delete login_admin.controller.js

--- panoptico_node/routes/login_admin.controller.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const login_adminController = require("../controllers/usuarios.controller");
-
-router.post("/login", login_adminController.login);
-
-module.exports = router;
