bf7823dcae7580ab9baa950ae2e900dafce8b2fa
KarolCardenasC <vanessacc102@gmail.com>
2022-11-06 19:45:52 -0500

    add funcion ingreso y registro al mismo tiempo de visitantes y estudiantes

--- panoptico_node/controllers/ingresos.controller.js
+++ panoptico_node/controllers/ingresos.controller.js
@@ -1,14 +1,15 @@
 const Ingreso = require("../models/ingresos.model");
+const Estudiante = require("../models/estudiantes.model");
+const Visitante = require("../models/visitantes.model");
+let date = new Date();
+date = new Date(date);
+date = date.toLocaleString();
 let response = {
   msg: "",
   exito: false,
 };
 
 exports.create = function (req, res) {
-  let date = new Date();
-  date = new Date(date);
-  date = date.toLocaleString();
-
   let ingreso = new Ingreso({
     fecha: date,
     id_ingreso: req.body.id_ingreso,
@@ -26,6 +27,98 @@ exports.create = function (req, res) {
 
     (response.exito = true),
       (response.msg = "El ingreso se guardó correctamente");
+    res.json(response);
+  });
+};
+
+exports.createEst = function (req, res) {
+  let cedula = req.body.cedula;
+  let nombre = req.body.nombre;
+  let telefono = req.body.telefono;
+  let email = req.body.email;
+
+  let estudiante = new Estudiante({
+    cedula: cedula,
+    nombre: nombre,
+    telefono: telefono,
+    email: email,
+  });
+
+  estudiante.save(function (err) {
+    if (err) {
+      console.error(err),
+        (response.exito = false),
+        (response.msg = "Error al ingresar al estudiante / ");
+      return;
+    }
+
+    (response.exito = true),
+      (response.msg = "El estudiante se ingresó correctamente / ");
+  });
+
+  let ingreso = new Ingreso({
+    fecha: date,
+    id_ingreso: cedula,
+    rol: "Estudiante",
+  });
+
+  ingreso.save(function (err) {
+    if (err) {
+      console.error(err),
+        (response.exito = false),
+        (response.msg = response.msg + "Error al guardar el ingreso");
+      res.json(response);
+      return;
+    }
+
+    (response.exito = true),
+      (response.msg = response.msg + "El ingreso se guardó correctamente");
+    res.json(response);
+  });
+};
+
+exports.createVis = function (req, res) {
+  let cedula = req.body.cedula;
+  let nombre = req.body.nombre;
+  let telefono = req.body.telefono;
+  let email = req.body.email;
+
+  let visitante = new Visitante({
+    cedula: cedula,
+    nombre: nombre,
+    telefono: telefono,
+    email: email,
+  });
+
+  visitante.save(function (err) {
+    if (err) {
+      console.error(err),
+        (response.exito = false),
+        (response.msg = "Error al ingresar al ingresar el visitante / ");
+      return;
+    }
+
+    (response.exito = true),
+      (response.msg = "El visitante se ingresó correctamente / ");
+  });
+
+  let ingreso = new Ingreso({
+    fecha: date,
+    id_ingreso: cedula,
+    rol: "Visitante",
+  });
+
+  ingreso.save(function (err) {
+    if (err) {
+      console.error(err),
+        (response.exito = false),
+        (response.msg = response.msg + "Error al guardar el ingreso");
+      res.json(response);
+      return;
+    }
+
+    (response.exito = true),
+      (response.msg = response.msg + "El ingreso se guardó correctamente");
     res.json(response);
   });
 };
--- panoptico_node/routes/ingresos.router.js
+++ panoptico_node/routes/ingresos.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const ingresosController = require("../controllers/ingresos.controller");
 
+router.post("/registros_estudiantes/", ingresosController.createEst);
+router.post("/registros_visitantes/", ingresosController.createVis);
 router.post("/", ingresosController.create);
 router.get("/", ingresosController.find);
 router.get("/:id_ingreso", ingresosController.findOne);
