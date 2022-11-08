0648fc509996440e001c773a9754145d807008a5
Lichundead <vallejoricaurte@gmail.com>
2022-11-08 10:56:47 -0500

    autorizacion test

--- panoptico_node/models/login_admin.model.js
+++ panoptico_node/models/login_admin.model.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Login_adminSchema = new Schema ({
-    email:{type:String, required: true, max: 100},
-    contraseña:{type:String, required:true, max: 128},
+const Login_adminSchema = new Schema({
+  email: { type: String, required: true, max: 100 },
+  pass: { type: String, required: true, max: 128 },
 });
 
-module.exports = mongoose.model("login_admin", Login_adminSchema);
+module.exports = mongoose.model("login_admins", Login_adminSchema);
