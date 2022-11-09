ac22f5613c23daaba979e5dd4ca187abd36fdb92
KarolCardenasC <vanessacc102@gmail.com>
2022-11-09 17:30:48 -0500

    barra de menu para admin

---
+++ panoptico_front/src/components/navbar/navbarAdmin.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  Dropdown,
+  DropdownButton,
+  Nav,
+  Navbar,
+  Row,
+  Container,
+} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserTie } from "@fortawesome/free-solid-svg-icons";
+import "./navbarAdmin.css";
+
+export default class menuTablas extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Navbar expand="lg" fixed="top" id="navbar" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand id="titulo" href="#home">
+            <img
+              alt=""
+              src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/052/519/original/icono.png?1668024209"
+              width="45"
+              height="38"
+              className="d-inline-block align-bottom"
+            />{" "}
+            Panoptico-Tablas
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/admin/estudiantes">Estudiantes</Nav.Link>
+              <Nav.Link href="/admin/visitantes">Visitantes</Nav.Link>
+              <Nav.Link href="/admin/ingresos/">Ingresos</Nav.Link>
+            </Nav>
+            <DropdownButton
+              id="dropdown-basic-button"
+              variant="dark"
+              menuVariant="dark"
+              title="Admin"
+            >
+              <Dropdown.Header id="dropdown-header">
+                <Row>
+                  <FontAwesomeIcon icon={faUserTie} />
+                </Row>
+                <Row>#Administrador#</Row>
+              </Dropdown.Header>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-1">Cerrar Sesión</Dropdown.Item>
+            </DropdownButton>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
