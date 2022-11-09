e892013e5e3bf6e63aa4a27cbf43682208767ec6
KarolCardenasC <vanessacc102@gmail.com>
2022-11-09 17:32:37 -0500

    enrutamiento

---
+++ panoptico_front/src/components/router/router.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Ingresos from "../tablas/ingresos";
+import Estudiantes from "../tablas/estudiantes";
+import Visitantes from "../tablas/visitantes";
+
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/admin/ingresos" element={<Ingresos />}></Route>
+        <Route
+          exact
+          path="/admin/estudiantes"
+          element={<Estudiantes />}
+        ></Route>
+        <Route exact path="/admin/visitantes" element={<Visitantes />}></Route>
+
+        <Route
+          path="/"
+          element={<h1 style={{ marginTop: 300 }}>Página de Inicio</h1>}
+        ></Route>
+
+        <Route
+          path="/*"
+          element={
+            <h1 style={{ marginTop: 300 }}>
+              404
+              <br />
+              Página no encontrada
+            </h1>
+          }
+        ></Route>
+      </Routes>
+    </Router>
+  );
+}
