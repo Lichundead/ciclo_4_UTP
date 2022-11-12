893f3af79142f0a2e981229de754256fd46b19c5
Lichundead <vallejoricaurte@gmail.com>
2022-11-12 14:37:17 -0500

    fix react dom dependency

--- panoptico_front/src/components/router/router.js
+++ panoptico_front/src/components/router/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Inicio from "../inicio/inicio";
 import Ingresos from "../tablas/ingresos";
 import Estudiantes from "../tablas/estudiantes";
@@ -8,27 +8,22 @@ import Visitantes from "../tablas/visitantes";
 export default function AppRoutes() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/admin/ingresos/" element={<Ingresos />}></Route>
+      <Switch>
+        <Route exact path={["/admin/ingresos/"]} component={Ingresos} />
+        <Route exact path={["/admin/estudiantes/"]} component={Estudiantes} />
+        <Route exact path={["/admin/visitantes/"]} component={Visitantes} />
+        <Route exact path={["/"]} component={Inicio} />
         <Route
-          exact
-          path="/admin/estudiantes/"
-          element={<Estudiantes />}
-        ></Route>
-        <Route exact path="/admin/visitantes/" element={<Visitantes />}></Route>
-        <Route
-          path="/*"
-          element={
+          path={"*"}
+          component={() => (
             <h1 style={{ marginTop: 300 }}>
               404
               <br />
               Página no encontrada
             </h1>
-          }
-        ></Route>
-
-        <Route exact path="/" element={<Inicio />}></Route>
-      </Routes>
+          )}
+        />
+      </Switch>
     </Router>
   );
 }
