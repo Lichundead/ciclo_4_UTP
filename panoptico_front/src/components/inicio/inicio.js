f32f902c865b8f0f0bb85b4a5b24e0e2173bee9f
Lichundead <vallejoricaurte@gmail.com>
2022-11-15 16:54:31 -0500

    index work in process

--- panoptico_front/src/components/inicio/inicio.js
+++ panoptico_front/src/components/inicio/inicio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Card, Button, Row } from "react-bootstrap";
 import NavInicio from "../navbar/navbarInicio";
 import "./inicio.css";
 
@@ -10,40 +11,49 @@ export default class inicio extends React.Component {
 
   render() {
     return (
-      <body>
+      <div className="fondo-inicio">
         <NavInicio />
-        <div className="containerPrincipal">
-          <div className="containerSecundario">
-            <div className="form-group">
-              <label>
-                <h2>Bienvenidos al Museo Panóptico de Ibagué</h2>
-              </label>
-              <br />
-              <label>
-                <h6>Selecciona una opción:</h6>
-              </label>
-              <br />
-              <button
-                id="boton-visitantes"
-                className="btn btn-primary"
-                onClick={() => this()}
-              >
-                Visitantes
-              </button>
-              <br />
+        <Container id="container">
+          <Row></Row>
+          <Row className="tarjetas-inicio">
+            <Card id="visitantes" style={{ width: "25rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg"
+              />
+              <Card.Body>
+                <Card.Title>
+                  <h2>Visitantes</h2>
+                </Card.Title>
+                <Card.Text>Por favor selecciona el botón ingresar</Card.Text>
+                <Card.Footer>
+                  <Button id="boton-visitantes" variant="light">
+                    Ingresar
+                  </Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
 
-              <br />
-              <button
-                id="boton-estudiantes"
-                className="btn btn-primary"
-                onClick={() => this()}
-              >
-                Estudiantes
-              </button>
-            </div>
-          </div>
-        </div>
-      </body>
+            <Card id="estudiantes" style={{ width: "25rem" }}>
+              <Card.Img
+                variant="top"
+                src="https://definicion.de/wp-content/uploads/2013/03/perro-1.jpg"
+              />
+              <Card.Body>
+                <Card.Title>
+                  <h2>Estudiantes</h2>
+                </Card.Title>
+                <Card.Text>Por favor selecciona el botón ingresar</Card.Text>
+                <Card.Footer>
+                  <Button id="boton-estudiantes" variant="light">
+                    Ingresar
+                  </Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Row>
+        </Container>
+      </div>
     );
   }
 }
